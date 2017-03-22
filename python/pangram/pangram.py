32e96b42764deed9f7b550a1710d07fecdee8dba
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-03-23 00:00:23 +0530

    submitted pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,15 @@
-def is_pangram():
-    pass
+def is_pangram(s):
+	count = 0
+	s = s.lower()
+	s = set(s)
+	flags_ = True
+	for x in s:
+		for y in 'qwertyuiopasdfghjklzxcvbnm':
+			if x==y:
+				count+=1
+	flags_
+	if count==26:
+		flags_ = True
+	else:
+		flags_ = False
+	return(flags_)
